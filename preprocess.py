5dbca485f0f4b8e296b75ae11dcc7cf5bc7f8c18
Graça <gracabot.dcc@gmail.com>
2017-06-10 17:52:24 -0300

    Add preprocess

--- preprocess.py
+++ preprocess.py
@@ -2,11 +2,21 @@
 
 import re
 import string
-re.sub( r'\bpara que\b', 'paraque', str )
-
 from unicodedata import normalize
+from reader import read
 
-def remover_acentos(txt, codif='utf-8'):
+SUBSTITUTION_SHEET = '1b2uzFDAL8QkidaYKiblU9QcYvovuV4mdPj38GhXLJwk'
+
+def preprocess(sentence):
+    sentence = remove_punctuation(sentence)
+    sentence = remove_accents(sentence)
+    sentence = remove_multiple_spaces(sentence)
+    sentence = substitution(sentence)
+    sentence = remove_small_words(sentence)
+    return sentence
+
+
+def remove_accents(txt, codif='utf-8'):
     return normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore')
 
 
@@ -15,25 +25,36 @@ def remove_punctuation(sentence):
     sentence = ''.join(ch for ch in sentence if ch not in punctuations)
     return sentence
 
+# TODO maybe a handmade dict with prepositions and articles would be more efficient
+def remove_small_words(sentence):
+    remover = re.compile(r'\W*\b\w{1,2}\b')
+    sentence = remover.sub('', sentence) 
+    return sentence
+
 
 def remove_multiple_spaces(sentence):
     return re.sub( '\s+', ' ', sentence ).strip()
 
 
+def substitute(sentence,tab):
+    data = read(SUBSTITUTION_SHEET,tab)
+    for row in data:
+        k = row[0]
+        value = row[1]
+        k = remove_punctuation(k)
+        k = remove_multiple_spaces(k)
+        k = remove_accents(k)
+        value = remove_punctuation(value)
+        value = remove_multiple_spaces(value)
+        value = remove_accents(value)
+        sentence = re.sub( r'\b'+k+r'\b', value, sentence )
+    return sentence
+
+
 def substitution(sentence):
-    # TODO substitute words
+    sentence = substitute(sentence,'siglas')
+    sentence = substitute(sentence,'academico')
+    sentence = substitute(sentence,'abreviacoes')
+    sentence = substitute(sentence,'conjuntos')
     return sentence
 
-
-# TODO maybe a handmade dict with prepositions and articles would be more efficient
-def remove_small_words(sentence):
-    remover = re.compile(r'\W*\b\w{1,2}\b'} 
-    return sentence
-
-
-def preprocess(sentence):
-    remove_punctuation(sentence)
-    remove_multiple_spaces(sentence)
-    substitution(sentence)
-    remove_small_words(sentence)
-    return sentence
--- reader.py
+++ reader.py
@@ -3,19 +3,12 @@ import httplib2
 from apiclient import discovery
 from oauth2client.service_account import ServiceAccountCredentials
 
-
-offset = 100
-
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-APPLICATION_NAME = 'Google Sheets API Python Quickstart'
-
-
 def get_credentials():
     scope = ['https://spreadsheets.google.com/feeds']
     credentials = ServiceAccountCredentials.from_json_keyfile_name('../cred.json', scope)
     return credentials
 
-def read (spreadsheetId):
+def read (spreadsheetId, tab):
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
@@ -23,7 +16,7 @@ def read (spreadsheetId):
     service = discovery.build('sheets', 'v4', http=http,
                               discoveryServiceUrl=discoveryUrl)
 
-    rangeName = 'pergunta'
+    rangeName = tab
     result = service.spreadsheets().values().get(
         spreadsheetId=spreadsheetId, range=rangeName).execute()
     values = result.get('values', [])
