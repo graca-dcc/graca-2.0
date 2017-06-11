67e1d524565e72e63671bb707d5b9a179a8acea8
Graça <gracabot.dcc@gmail.com>
2017-06-11 17:47:21 -0300

    test

--- index.py
+++ index.py
@@ -29,7 +29,7 @@ def webhook():
             text = data['entry'][0]['messaging'][0]['message']['text']
             sender = data['entry'][0]['messaging'][0]['sender']['id']
             ans = get_answer(classifier,text)
-            payload = {'recipient': {'id': sender}, 'message': {'text': ans}}
+            payload = {'recipient': {'id': sender}, 'message': {'text': 'hello!'}}
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
         except Exception as e:
             print(traceback.format_exc())
