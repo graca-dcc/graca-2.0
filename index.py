286406567ec3b96a18899b25184aad7a29fc6e82
Graça <gracabot.dcc@gmail.com>
2017-06-11 17:51:37 -0300

    test

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from classifier import get_answer
 from classifier import create_classifier
 
 variaveis = dict()
-classifier = create_classifier()
+#classifier = create_classifier()
 def get_nome (name, data):
     name = data['entry'][0]['messaging'][0]['sender']['name']
     msg = msg.replace("getNome",name)
@@ -28,7 +28,7 @@ def webhook():
             data = json.loads(request.data.decode())
             text = data['entry'][0]['messaging'][0]['message']['text']
             sender = data['entry'][0]['messaging'][0]['sender']['id']
-            ans = get_answer(classifier,text)
+            #ans = get_answer(classifier,text)
             payload = {'recipient': {'id': sender}, 'message': {'text': 'hello!'}}
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
         except Exception as e:
