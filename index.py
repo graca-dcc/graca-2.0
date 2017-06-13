d49c0cb46bf88aa6f470042279562b0020e000b9
Graça <gracabot.dcc@gmail.com>
2017-06-13 03:08:09 -0300

    test

--- index.py
+++ index.py
@@ -25,10 +25,10 @@ app = Flask(__name__)
 #classifier = joblib.load('cls.pkl')
 #sub_dict = joblib.load('sd.pkl')
 
-classifier = None
-answers = None
-sub_dict = None
-variables = None
+#classifier = None
+#answers = None
+#sub_dict = None
+#variables = None
 
 def load_variables():
     variables = dict()
@@ -48,7 +48,7 @@ def get_nome (name, data):
     return msg
 
 
-def substitute_variables(msg):
+def substitute_variables(variables, msg):
     for v in variables:
         msg = re.sub(r'\b'+v+r'\b',variables[v],msg)
     return msg
@@ -61,9 +61,13 @@ def webhook():
             data = json.loads(request.data.decode())
             text = data['entry'][0]['messaging'][0]['message']['text']
             sender = data['entry'][0]['messaging'][0]['sender']['id']
-            global classifier
+            #global classifier
+            classifier = app.config['classifier']
+            answers = app.config['answers']
+            sub_dict = app.config['sub_dict']
+            variables = app.config['variables']
             ans = get_answer(classifier,answers,sub_dict,text)
-            ans = substitute_variables(ans)
+            ans = substitute_variables(variables,ans)
             payload = {'recipient': {'id': sender}, 'message': {'text': ans}}
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
         except Exception as e:
@@ -75,10 +79,9 @@ def webhook():
     return 'Nothing'
 
 if __name__ == '__main__':
-    global variables
-    global classifier
-    global answers
-    global sub_dict
-    variables = load_variables()
     classifier, answers, sub_dict = create_classifier()
+    app.config['classifier'] = classifier
+    app.config['answers'] = answers
+    app.config['sub_dict'] = sub_dict
+    app.config['variables'] = load_variables()
     app.run(debug=True)
